4ce72c3ea58d114e71bd1290b31f84192c6b4a8b
KRothfus <krothfus92@gmail.com>
2025-08-13 07:09:28 -0700

    In the middle of adding the feeds followed feature TODO: need to finish implementing the logic for updating followed feeds in the database

--- src/feedcommands.ts
+++ src/feedcommands.ts
@@ -1,7 +1,8 @@
 import { readConfig } from "./config";
 import { db } from "./lib/db";
 import { getUser, getUserById } from "./lib/db/queries/users";
-import { feeds, users } from "./lib/db/schema";
+import { feed_follows, feeds, users } from "./lib/db/schema";
+import { eq } from "drizzle-orm";
 
 export async function createFeed(feedName: string, feedURL: string, currentUserId: string) {
     const [result] = await db.insert(feeds).values({name: feedName, url: feedURL, user_id: currentUserId}).returning();
@@ -45,4 +46,76 @@ export async function feedsHandler() {
     }    
 }
 
+export type FeedFollow = typeof feed_follows.$inferSelect;
+
+export async function createFeedFollow(feedFollows:FeedFollow, userId: string) {
+    const [newFeedFollow] = await db.insert(feed_follows).values(feedFollows).returning();
+
+    const result = await db.select({
+        id: feed_follows.id,
+        createdAt: feed_follows.createdAt,
+        updatedAt: feed_follows.updatedAt,
+        user_id: feed_follows.user_id,
+        feed_id: feed_follows.feed_id,
+        feed_name: feeds.name,
+        user_name: users.name
+    })
+    .from(feed_follows).innerJoin(feeds, eq(feed_follows.feed_id, feeds.id))
+    .innerJoin(users, eq(feed_follows.user_id, users.id))
+    .where(eq(feed_follows.id, newFeedFollow.id));
+    if (result.length === 0) {
+        throw new Error("Failed to create feed follow");
+    }
+    return result;
+}
+
+export async function follow(url: string) {
+    const currentUser = readConfig().currentUserName;
+    const currentUserId = (await getUser(currentUser))?.id;
+    if (!currentUserId) {
+        throw new Error(`User ${currentUser} not found`);
+    }
+
+    const feed = await db.select().from(feeds).where(eq(feeds.url, url)).limit(1);
+   
+
+    const feedFollowed: FeedFollow = {
+        user_id: currentUserId,
+        feed_id: feed[0].id,
+        id: "",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    };
+
+    await createFeedFollow(feedFollowed, currentUserId);
+}
+
+export async function getFeedFollowsForUser(userName:string) {
+    const userID = (await getUser(userName))?.id;
+    if (!userID) {
+        throw new Error(`User ${userName} not found`);
+    }
+    const feedFollows = await db.select().from(feed_follows).where(eq(feed_follows.user_id, userID));
+    if (feedFollows.length === 0) {
+        throw new Error(`No feed follows found for user ${userName}`);
+    }
+    return feedFollows;
+    
+}
+
+export async function following() {
+    const currentUser = readConfig().currentUserName;
+    const currentUserId = (await getUser(currentUser))?.id;
+    if (!currentUserId) {
+        throw new Error(`User ${currentUser} not found`);
+    }
+    const feedsFollowed = await getFeedFollowsForUser(currentUser);
+    if (!feedsFollowed) {
+        throw new Error(`No feeds followed by user ${currentUser}`);
+    }
+    
+    for (const feedFollow of feedsFollowed) {
+        console.log(`* Feed ID: ${feedFollow.feed_id}`);
+    }
+}
 
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -21,3 +21,14 @@ export const feeds = pgTable("feeds", {
   url: text("url").notNull().unique(),
   user_id: uuid('user_id').notNull().references(() => users.id, { onDelete: 'cascade' }),
 })
+
+export const feed_follows = pgTable("feeds_follows", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  user_id: uuid('user_id').notNull().references(() => users.id, { onDelete: 'cascade' }).unique(),
+  feed_id: uuid('feed_id').notNull().references(() => feeds.id, { onDelete: 'cascade' }).unique(),
+})
