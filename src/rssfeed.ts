4a345df74936be779983f3b2c9d9616f8bc40350
KRothfus <krothfus92@gmail.com>
2025-09-03 07:09:40 -0700

    Working through generating the migration files.

--- src/rssfeed.ts
+++ src/rssfeed.ts
@@ -1,4 +1,7 @@
 import { XMLParser } from "fast-xml-parser";
+import { db } from "./lib/db";
+import { feeds } from "./lib/db/schema";
+import { eq, sql } from "drizzle-orm";
 
 type RSSItem = {
   title: string;
@@ -66,12 +69,83 @@ export async function fetchFeed(feedURL: string): Promise<RSSFeed> {
 }
 // keep the streak!
 
-export async function agg() {
-  try {
-    const url = "https://www.wagslane.dev/index.xml";
-    const rssFeed = await fetchFeed(url);
-    console.log(JSON.stringify(rssFeed, null, 2));
-  } catch (error) {
-    console.error("Error fetching or parsing feed:", error);
+
+export async function agg(time_between_reqs: string) {
+const delay = parseDuration(time_between_reqs);
+console.log(`Collecting feeds every ${time_between_reqs}`);
+try{
+await scrapeFeeds();
+} catch (e) {
+  console.log(`Error during initial scrape: ${e}`);
+}
+
+const interval = setInterval(async () => {
+  try{
+  await scrapeFeeds();
+  } catch (e) {
+    console.log(`Error during scheduled scrape: ${e}`);
   }
+}, delay);
+
+await new Promise<void>((resolve) => {
+  process.on("SIGINT", () => {
+    console.log("Shutting down feed aggregator...");
+    clearInterval(interval);
+    resolve();
+  });
+});
+}
+
+function parseDuration(duration: string): number {
+  const regex = /^(\d+)(ms|s|m|h)$/;
+const match = duration.match(regex);
+if (!match) {
+  throw new Error("Invalid duration format");
+}
+const value = parseInt(match[1], 10);
+const unit = match[2];
+switch (unit) {
+  case "ms":
+    return value;
+  case "s":
+    return value * 1000;
+  case "m":
+    return value * 60 * 1000;
+  case "h":
+    return value * 60 * 60 * 1000;
+  default:
+    throw new Error("Invalid time unit");
+}
+}
+
+export async function markFeedFetched(id: string) {
+  await db.update(feeds).set({ last_fetched_at: new Date(), updatedAt: new Date() }).where(eq(feeds.id,id));
+}
+
+export async function getNextFeedToFetch() {
+  const feed_to_fetch = await db.select().from(feeds).orderBy(sql`${feeds.last_fetched_at} asc nulls first`).limit(1);
+  return feed_to_fetch[0];
+}
+
+export async function scrapeFeeds(){
+  // get next feed to fetch from db
+  
+  const feed = await getNextFeedToFetch();
+ if (!feed) { 
+    console.log(`No feeds to fetch.`);
+    return;
+  }
+  // fetch the feed using the url (func already written)
+  try{
+    const rssFeed = await fetchFeed(feed.url);
+    // iterate over items in the feed and print titles to the console.
+    rssFeed.channel.item.forEach((item) => {
+      console.log(item.title);
+    });
+  } catch (e) {
+    console.log(`Error fetching feed ${feed.url}: ${e}`);
+    return;
+  }
+  // mark it as fetched
+  markFeedFetched(feed.id);
 }
