e23e954a5f6783358e73a5bc713f3065be7dc639
KRothfus <krothfus92@gmail.com>
2025-09-13 22:54:04 -0700

    adfa

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,8 +3,10 @@ import postgres from "postgres";
 
 import * as schema from "./schema";
 import { readConfig } from "../../config";
+import { listenerCount } from "process";
 
 const config = readConfig();
 const conn = postgres(config.dbUrl);
 export const db = drizzle(conn, { schema });
 // oh hello...oh hello...
+// perfect
